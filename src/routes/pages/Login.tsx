10a76cf7ceec4990cc76ba0b76440bbdca4afb6a
naiiiden <na4ko359@gmail.com>
2026-08-04 14:34:06 +0300

    login markup

--- src/routes/pages/Login.tsx
+++ src/routes/pages/Login.tsx
@@ -1,3 +1,18 @@
 export default function Login() {
-  return <p className="text-9xl">hi login plz</p>;
+  return (
+    <main>
+      <h1>Login</h1>
+      <form>
+        <label htmlFor="username">Username:</label>
+        <input type="text" id="username" placeholder="Enter your username" />
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          placeholder="Enter your password"
+        />
+        <button>Login</button>
+      </form>
+    </main>
+  );
 }
