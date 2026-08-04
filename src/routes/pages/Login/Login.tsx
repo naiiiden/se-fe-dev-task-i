bb92b84653ab73dc39022c23471f1559b02ff934
naiiiden <na4ko359@gmail.com>
2026-08-05 00:36:40 +0300

    form & redirect

--- src/routes/pages/Login/Login.tsx
+++ src/routes/pages/Login/Login.tsx
@@ -1,15 +1,36 @@
+import { useForm } from "react-hook-form";
+import { LoginSchema, type LoginSchemaType } from "./LoginSchema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useNavigate } from "react-router-dom";
+
 export default function Login() {
+  const navigate = useNavigate();
+
+  const form = useForm<LoginSchemaType>({
+    resolver: zodResolver(LoginSchema),
+  });
+
+  const onSubmit = () => {
+    navigate("/table");
+  };
+
   return (
     <main>
       <h1>Login</h1>
-      <form>
+      <form onSubmit={form.handleSubmit(onSubmit)}>
         <label htmlFor="username">Username:</label>
-        <input type="text" id="username" placeholder="Enter your username" />
+        <input
+          type="text"
+          id="username"
+          placeholder="Enter your username"
+          {...form.register("username")}
+        />
         <label htmlFor="password">Password:</label>
         <input
           type="password"
           id="password"
           placeholder="Enter your password"
+          {...form.register("password")}
         />
         <button>Login</button>
       </form>
