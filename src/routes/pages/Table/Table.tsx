1328e7fb196d4eb4c39ec4e408c7d16b94284ece
naiiiden <na4ko359@gmail.com>
2026-08-05 12:34:47 +0300

    isLoading, Spinner

--- src/routes/pages/Table/Table.tsx
+++ src/routes/pages/Table/Table.tsx
@@ -7,51 +7,55 @@ import {
   DataGridRow,
   FluentProvider,
   webLightTheme,
+  Spinner,
 } from "@fluentui/react-components";
 import { columns } from "./table-columns";
 import { usePeople } from "./fetchPeople";
 
 export default function Table() {
-  const { data, page, setPage } = usePeople();
+  const { data, isLoading, page, setPage } = usePeople();
 
-  console.log(2, page);
-  console.log(3, data);
+  console.log(1, page);
 
   return (
     <FluentProvider theme={webLightTheme}>
-      <DataGrid
-        items={data?.results ?? []}
-        columns={columns}
-        getRowId={(item) => item.url}
-        focusMode="composite"
-      >
-        <DataGridHeader>
-          <DataGridRow>
-            {({ renderHeaderCell }) => (
-              <DataGridHeaderCell>{renderHeaderCell()}</DataGridHeaderCell>
-            )}
-          </DataGridRow>
-        </DataGridHeader>
-        <DataGridBody>
-          {({ item, rowId }) => (
-            <DataGridRow key={rowId}>
-              {({ renderCell }) => (
-                <DataGridCell>{renderCell(item)}</DataGridCell>
+      {isLoading ? (
+        <Spinner label="Loading Star Wars data..." />
+      ) : (
+        <DataGrid
+          items={data?.results ?? []}
+          columns={columns}
+          getRowId={(item) => item.url}
+          focusMode="composite"
+        >
+          <DataGridHeader>
+            <DataGridRow>
+              {({ renderHeaderCell }) => (
+                <DataGridHeaderCell>{renderHeaderCell()}</DataGridHeaderCell>
               )}
             </DataGridRow>
-          )}
-        </DataGridBody>
-      </DataGrid>
+          </DataGridHeader>
+          <DataGridBody>
+            {({ item, rowId }) => (
+              <DataGridRow key={rowId}>
+                {({ renderCell }) => (
+                  <DataGridCell>{renderCell(item)}</DataGridCell>
+                )}
+              </DataGridRow>
+            )}
+          </DataGridBody>
+        </DataGrid>
+      )}
 
       <button
-        disabled={data?.previous === null}
+        disabled={isLoading || data?.previous === null}
         onClick={() => setPage(page - 1)}
         className={data?.previous === null ? "text-red-600" : ""}
       >
         Previous
       </button>
       <button
-        disabled={data?.next === null}
+        disabled={isLoading || data?.next === null}
         onClick={() => setPage(page + 1)}
         className={data?.next === null ? "text-red-600" : ""}
       >
